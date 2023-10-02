83ad79474db6c6a4cf1173b96be06182d1ec8211
Nimanga <nimangasupun97@gmail.com>
2023-10-02 09:12:27 +0530

    add axios

--- DictionaryFront/src/components/dataBaseConnection.js
+++ DictionaryFront/src/components/dataBaseConnection.js
@@ -1,9 +1,14 @@
+import axios from 'axios';
 import React, {useEffect, useState} from 'react';
 import {Alert, Text, View, ActivityIndicator, Modal} from 'react-native';
 import SQLite from 'react-native-sqlite-storage';
 
 const DatabaseConnection = ({onEnWordsListed, onSnWordsListed}) => {
   const [loading, setLoading] = useState(true);
+  const [getVersion, setGetVersion] = useState('');
+  const [remoteVersion, setRemoteVersion] = useState('');
+  const [enRemotedate, setEnRemoteData] = useState([]);
+  const [snRemotedate, setSnRemoteData] = useState([]);
 
   useEffect(() => {
     const db = SQLite.openDatabase(
@@ -19,18 +24,25 @@ const DatabaseConnection = ({onEnWordsListed, onSnWordsListed}) => {
 
         db.transaction(tx => {
           tx.executeSql(
-            "SELECT name FROM sqlite_master WHERE type='table' AND name IN ('enWords', 'snWords')",
+            "SELECT name FROM sqlite_master WHERE type='table' AND name IN ('enWords', 'snWords' , 'versionInfo')",
             [],
             (tx, resultSet) => {
-              if (resultSet.rows.length === 2) {
+              if (resultSet.rows.length === 3) {
                 // Both tables exist, no need to insert data from JSON
                 console.log('Tables already exist');
+                getVersion();
+                getRemoteVersion();
+                checkVersion();
                 listSnWords();
                 listEnWords();
               } else {
                 // At least one table is missing, insert data from JSON
                 insertDataFromJSON1();
                 insertDataFromJSON2();
+                versionDataFromJson();
+                getVersion();
+                getRemoteVersion();
+                checkVersion();
                 listSnWords();
                 listEnWords();
               }
@@ -40,14 +52,33 @@ const DatabaseConnection = ({onEnWordsListed, onSnWordsListed}) => {
             },
           );
         });
-
-        // listEnWords();
-        // listSnWords();
       },
       error => {
         console.log('Database Error', error);
       },
     );
+
+    const getRemoteVersion = () => {
+      axios
+        .get('http://10.0.2.2:8050/dictionary/version')
+        .then(res => {
+          setRemoteVersion(res.data[0].version);
+        })
+        .catch(error => {
+          console.log(error.message);
+        });
+    };
+
+    const checkVersion = () => {
+      if (!remoteVersion == getVersion) {
+        getEnData();
+        getSnData();
+        dropTable();
+        updateRemoteEnData();
+        updateRemoteSnData();
+      } else console.log('no any update');
+    };
+
     //inset data to SQLite database from (sn2en)JSON file
     const insertDataFromJSON1 = async () => {
       console.log('Data insert');
@@ -101,6 +132,73 @@ const DatabaseConnection = ({onEnWordsListed, onSnWordsListed}) => {
       }
     };
 
+    const versionDataFromJson = async () => {
+      try {
+        const versionData = require('../../android/app/src/main/assets/version.json');
+
+        await db.transaction(async tx => {
+          await tx.executeSql(
+            'CREATE TABLE IF NOT EXISTS versionInfo ( id INTEGER PRIMARY KEY, version TEXT)',
+          );
+          await tx.executeSql('INSERT INTO versionInfo (version) VALUES (?)', [
+            versionData.version,
+          ]);
+
+          console.log('version Insert');
+        });
+      } catch (error) {
+        console.log('Error inserting data', error);
+      }
+    };
+
+    const updateRemoteEnData = async () => {
+      console.log('Data insert');
+      try {
+        const jsonData = enRemotedate;
+
+        await db.transaction(async tx => {
+          await tx.executeSql(
+            'CREATE TABLE IF NOT EXISTS enWords (id INTEGER PRIMARY KEY , word TEXT, definition TEXT)',
+            [],
+          );
+          for (const item of jsonData) {
+            await tx.executeSql(
+              'INSERT INTO snWords (word, definition) VALUES (?, ?)',
+              [item.word, JSON.stringify(item.definitions)],
+            );
+          }
+        });
+
+        console.log('Data inserted successfully');
+      } catch (error) {
+        console.log('Error inserting data:', error);
+      }
+    };
+
+    const updateRemoteSnData = async () => {
+      console.log('Data insert');
+      try {
+        const jsonData = snRemotedate;
+
+        await db.transaction(async tx => {
+          await tx.executeSql(
+            'CREATE TABLE IF NOT EXISTS snWords (id INTEGER PRIMARY KEY , word TEXT, definition TEXT)',
+            [],
+          );
+          for (const item of jsonData) {
+            await tx.executeSql(
+              'INSERT INTO snWords (word, definition) VALUES (?, ?)',
+              [item.word, JSON.stringify(item.definitions)],
+            );
+          }
+        });
+
+        console.log('Data inserted successfully');
+      } catch (error) {
+        console.log('Error inserting data:', error);
+      }
+    };
+
     const dropTable = async () => {
       console.log('DropTable');
       try {
@@ -114,6 +212,31 @@ const DatabaseConnection = ({onEnWordsListed, onSnWordsListed}) => {
         // insertDataFromJSON2();
       } catch (error) {
         console.error('Error dropping table:', error);
+      }
+    };
+
+    const getVersion = () => {
+      try {
+        let sql = 'SELECT version FROM versionInfo';
+
+        db.transaction(async tx => {
+          tx.executeSql(
+            sql,
+            [],
+            (_, {rows}) => {
+              if (rows.length > 0) {
+                const version = rows.item(0).version;
+
+                setGetVersion(version);
+              }
+            },
+            error => {
+              console.log('Get version error', error);
+            },
+          );
+        });
+      } catch (error) {
+        console.log('Error getting version ', error);
       }
     };
 
@@ -175,7 +298,33 @@ const DatabaseConnection = ({onEnWordsListed, onSnWordsListed}) => {
       }
     };
   }, []);
+
+  const getEnData = () => {
+    axios
+      .get('http://10.0.2.2:8050/dictionary/en2sn')
+      .then(res => {
+        setEnRemoteData(res.data);
+      })
+      .catch(err => {
+        console.log(err.message);
+      });
+  };
+
+  const getSnData = () => {
+    axios
+      .get('http://10.0.2.2:8050/dictionary/sn2en')
+      .then(res => {
+        setSnRemoteData(res.data);
+      })
+      .catch(err => {
+        console.log(err.message);
+      });
+  };
+
   console.log(loading);
+  console.log(getVersion);
+  console.log(remoteVersion);
+
   return (
     <>
       {loading ? (
