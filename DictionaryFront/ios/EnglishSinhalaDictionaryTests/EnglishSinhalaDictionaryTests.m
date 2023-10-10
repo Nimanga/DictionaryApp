f08782874b2fa8c476cb024425bcf487f54357db
Nimanga <nimangasupun97@gmail.com>
2023-10-10 09:54:12 +0530

    rename

--- DictionaryFront/ios/EnglishSinhalaDictionaryTests/EnglishSinhalaDictionaryTests.m
+++ DictionaryFront/ios/EnglishSinhalaDictionaryTests/EnglishSinhalaDictionaryTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface DictionaryFrontTests : XCTestCase
+@interface EnglishSinhalaDictionaryTests : XCTestCase
 
 @end
 
-@implementation DictionaryFrontTests
+@implementation EnglishSinhalaDictionaryTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
